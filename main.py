33c379f22b6504016ce30754f0bb26b0f810b4ce
Tristan Zine <zine96@protonmail.com>
2022-04-24 23:28:44 -0400

    - fixed tax calculation on clothes from MA. Now correctly calculates difference to tax when over a subtotal of 175 in clothes from MA. - calculation now occurs in cart checkout rather than within the data class. Before it would only calculate on a per item basis and discount other cart items in the list.

--- main.py
+++ main.py
@@ -34,9 +34,7 @@ class CartItem:
 
     def total_cost_clothing(self) -> float:
         subtotal = self.item_price * self.quantity
-        if self.state_purchased.lower() == "ma" and subtotal > 175:
-            return subtotal + ((subtotal - 175) * .0625)
-        elif self.state_purchased.lower() == "me":
+        if self.state_purchased.lower() == "me":
             return subtotal + (subtotal * .0525)
         else:
             return subtotal
@@ -47,15 +45,20 @@ def create_cart_items() -> list[CartItem]:
     cart_items = [CartItem('bread', 'Wic Eligible Food', 3.99, 'ma', 2),
                   CartItem('milk', 'Wic Eligible Food', 4.79, 'ma', 1),
                   CartItem('eggs', 'Wic Eligible Food', 2.89, 'me', 1),
-                  CartItem('jeans', 'clothing', 29.99, 'ma', 1)]
+                  CartItem('jeans', 'clothing', 29.99, 'ma', 1),
+                  CartItem('hoodie', 'clothing', 79.99, 'ma', 2)]
     return cart_items
 
 
 def cart_total(cart_items: list[CartItem]) -> float:
     total = 0
+    ma_clothing_subtotal = 0
     for item in cart_items:
+        if item.item_type.lower() == 'clothing' and item.state_purchased.lower() == 'ma':
+            ma_clothing_subtotal += item.total_cost()
         total += item.total_cost()
-    print(total)
+    if ma_clothing_subtotal > 175:
+        total += ((ma_clothing_subtotal - 175) * .0625)
     return total
 
 
